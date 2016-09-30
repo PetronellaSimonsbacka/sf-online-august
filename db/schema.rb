f654d49565f0f8a35894d9c5fb8abaca533d704b
PetronellaSimonsbacka <gussimonpe@student.gu.se>
2016-09-30 15:59:17 +0200

    Migrated db/schema

--- db/schema.rb
+++ db/schema.rb
@@ -58,6 +58,7 @@ ActiveRecord::Schema.define(version: 20160930074546) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "",         null: false
     t.string   "encrypted_password",     default: "",         null: false
